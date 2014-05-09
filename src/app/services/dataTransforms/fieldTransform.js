93dbeceb95dca9c035938a87f0683d45e7e119f4
nelson <nelson@wikia-inc.com>
2014-05-09 10:48:33 -0700

    adding param for match against in fieldTransform

--- src/app/services/dataTransforms/fieldTransform.js
+++ src/app/services/dataTransforms/fieldTransform.js
@@ -4,13 +4,14 @@ define([
 ],
   function(ng, _) {
     ng.module('kibana.services').service('fieldTransform', function(dataTransform) {
-      this.transform = function(results, search, fieldName, returnIndex) {
+      this.transform = function(results, search, fieldName, returnIndex, matchAgainst) {
         var regex = dataTransform.parseRegex(search),
           returnIndex = returnIndex || 0,
+          matchAgainst = matchAgainst || '@message',
           matches;
 
         _.forEach(results.hits, function(hit) {
-          matches = hit._source['@message'].match(regex);
+          matches = hit._source[matchAgainst].match(regex);
 
           if (matches != null) {
             hit._source[fieldName] = matches[returnIndex];
